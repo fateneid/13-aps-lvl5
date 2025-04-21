7b143c2c8c5bb3dba9a2ed1e04f404dc3f957220
Faten Eid <87611899+fateneid@users.noreply.github.com>
2025-04-21 20:41:18 +0200

    Extensions 1 to 7

--- clsMyQueue.h
+++ clsMyQueue.h
@@ -38,11 +38,6 @@ public:
 		return _MyList.IsEmpty();
 	}
 
-	bool IsEmpty()
-	{
-		return _MyList.IsEmpty();
-	}
-
 	T front()
 	{
 		return _MyList.GetItem(0);
@@ -53,6 +48,40 @@ public:
 		return _MyList.GetItem(Size() - 1);
 	}
 
+	T GetItem(int index)
+	{
+		return _MyList.GetItem(index);
+	}
+
+	void Reverse()
+	{
+		_MyList.Reverse();
+	}
+
+	void UpdateItem(int index, T new_value)
+	{
+		_MyList.UpdateItem(index, value);
+	}
+
+	void InsertAfter(int index, T value)
+	{
+		_MyList.InsertAfter(index, value);
+	}
+
+	void InsertAtFront(T value)
+	{
+		_MyList.InsertAtBeginning(value);
+	}
+
+	void InsertAtBack(T value)
+	{
+		_MyList.InsertAtEnd(value);
+	}
+
+	void Clear()
+	{
+		_MyList.Clear();
+	}
 
 };
 
