31e98323677c2507ef24c0b3430e9755cb0d7f3f
Faten Eid <87611899+fateneid@users.noreply.github.com>
2025-04-22 23:15:16 +0200

    Extension 01:  Resize()

--- clsDynamicArray.h
+++ clsDynamicArray.h
@@ -8,29 +8,30 @@ class clsDynamicArray
 protected:
 
 	int _Size = 0;
+	T* _TempArray;
 
 public:
 
-	T* DynamicArray;
+	T* OriginalArray;
 
 	clsDynamicArray(int Size = 0)
 	{
 		if (Size < 0) Size = 0;
 
 		_Size = Size;
-		DynamicArray = new T[_Size];
+		OriginalArray = new T[_Size];
 	}
 
 	~clsDynamicArray()
 	{
-		delete[] DynamicArray;
+		delete[] OriginalArray;
 	}
 
 	bool SetItem(int index, T value)
 	{
 		if (index < 0 || index >= _Size) return false;
 
-		DynamicArray[index] = value;
+		OriginalArray[index] = value;
 		return true;
 	}
 
@@ -47,10 +48,28 @@ public:
 	void PrintList()
 	{
 		for (int i = 0; i < _Size; i++) {
-			cout << DynamicArray[i] << " ";
+			cout << OriginalArray[i] << " ";
 		}
 		cout << "\n";
 	} 
+
+	void Resize(int new_size)
+	{
+		if (new_size < 0) new_size = 0;
+
+		_TempArray = new T[new_size];
+
+		if (new_size < _Size) _Size = new_size;
+
+		for (int i = 0; i < _Size; i++) {
+			_TempArray[i] = OriginalArray[i];
+		}
+
+		_Size = new_size;
+		delete[] OriginalArray;
+		OriginalArray = _TempArray;
+
+	}
 
 };
 
